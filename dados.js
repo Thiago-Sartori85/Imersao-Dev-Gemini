21088d059abc4e9b9f204d54bf067c25b69e70d5
Thiago-Sartori <inovadornato@gmail.com>
2024-09-08 15:39:40 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -2,26 +2,32 @@ let dados = [
     {
         titulo: "Saint Seiya",
         descricao: "Foi um dos primeiros animes a fazer sucesso no Brasil, transmitido pela extinta Manchete na década de 80. Sua história épica de cavaleiros que lutam para proteger a deusa Atena cativou o público brasileiro, tornando-se um marco na história da animação japonesa no país.",
-        link: "https://pt.wikipedia.org/wiki/Os_Cavaleiros_do_Zod%C3%ADaco"
+        link: "https://pt.wikipedia.org/wiki/Os_Cavaleiros_do_Zod%C3%ADaco",
+        //As TAGS incluidas para melhorar experiencia do usuário na busca das informações
+        tags: "mitologia grega cavaleiros shonen anos 80 clássico ação fantasia amizade superação Seiya Hyoga Shiryu"
     },
     {
         titulo: "Dragon Ball",
         descricao: "Creado por Akira Toriyama, Dragon Ball acompanha as aventuras de Goku, um jovem saiyan com uma força incrível, em busca das Esferas do Dragão, artefatos capazes de realizar qualquer desejo. O anime se tornou um fenômeno mundial, marcando gerações.",
-        link: "https://pt.wikipedia.org/wiki/Dragon_Ball"
+        link: "https://pt.wikipedia.org/wiki/Dragon_Ball",
+        tags: "artes marciais shonen aventura saiyan esferas do dragão Goku Vegeta Bulma amizade família, poder"
     },
     {
         titulo: "Hokuto no Ken",
         descricao: "Conhecido no Brasil como Punho da Estrela do Norte, Hokuto no Ken é um anime pós-apocalíptico que narra a história de Kenshiro, um mestre em artes marciais que busca vingança pela morte de sua amada.",
-        link: "https://pt.wikipedia.org/wiki/Hokuto_no_Ken"
+        link: "https://pt.wikipedia.org/wiki/Hokuto_no_Ken",
+        tags: "pós-apocalíptico artes marciais vingança shonen Kenshiro Raoh Shin drama ação sobrevivência"
     },
     {
         titulo: "City Hunter",
         descricao: "City Hunter, é um caçador de recompensas que trabalha em Tóquio, resolvendo casos e protegendo os mais fracos. O anime é conhecido por sua mistura de ação, comédia e romance.",
-        link: "https://pt.wikipedia.org/wiki/City_Hunter"
+        link: "https://pt.wikipedia.org/wiki/City_Hunter",
+        tags: "ação comédia romance caçador de recompensas Tóquio anos 80 Ryo Saeba Kaori Makimura mistério"
     },
     {
         titulo: "Sailor Moon",
         descricao: "Uma das mais populares séries de anime para meninas, Sailor Moon acompanha as aventuras de Usagi Tsukino, uma jovem que descobre ser a líder das Sailor Senshi, guerreiras que protegem a Terra do mal.",
-        link: "https://pt.wikipedia.org/wiki/Sailor_Moon"
+        link: "https://pt.wikipedia.org/wiki/Sailor_Moon",
+        tags: "magical girl shonen romance Sailor Senshi Usagi Tsukino amizade amor transformação anos 90"
     }
 ];
