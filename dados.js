b39d405b1d1f2df692c9f56d75795ccef460b169
Thiago-Sartori <inovadornato@gmail.com>
2024-09-08 17:31:18 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -29,5 +29,47 @@ let dados = [
         descricao: "Uma das mais populares séries de anime para meninas, Sailor Moon acompanha as aventuras de Usagi Tsukino, uma jovem que descobre ser a líder das Sailor Senshi, guerreiras que protegem a Terra do mal.",
         link: "https://pt.wikipedia.org/wiki/Sailor_Moon",
         tags: "magical girl shonen romance Sailor Senshi Usagi Tsukino amizade amor transformação anos 90"
-    }
+    },
+    {
+        titulo: "Demon Slayer: Kimetsu no Yaiba",
+        descricao: "Um jovem caçador de demônios busca vingança pela morte de sua família e uma cura para sua irmã transformada em demônio.",
+        link: "https://myanimelist.net/anime/38524/Kimetsu_no_Yaiba",
+        tags: "ação fantasia shounen"
+    },
+    {
+        titulo: "Jujutsu Kaisen",
+        descricao: "Um estudante do ensino médio descobre um mundo oculto de maldições e se junta a uma escola de exorcistas.",
+        link: "https://myanimelist.net/anime/40030/Jujutsu_Kaisen",
+        tags: "ação sobrenatural shounen"
+    },
+    {
+        titulo: "Fullmetal Alchemist: Brotherhood",
+        descricao: "Dois irmãos alquimistas buscam restaurar seus corpos após um experimento fracassado. Uma jornada épica em busca da Pedra Filosofal.",
+        link: "https://myanimelist.net/anime/5114/Fullmetal_Alchemist__Brotherhood",
+        tags: "aventura fantasia shounen steampunk"
+    },
+    {
+        titulo: "Mob Psycho 100",
+        descricao: "Um jovem com poderes psíquicos tenta levar uma vida normal enquanto lida com os desafios de controlar suas habilidades.",
+        link: "https://myanimelist.net/anime/31043/Mob_Psycho_100",
+        tags: "comédia sobrenatura shounen"
+    },
+    {
+        titulo: "Violet Evergarden",
+        descricao: "Uma ex-soldado com um coração de aço se torna uma boneca automática que escreve cartas para outras pessoas, aprendendo sobre o amor e a perda.",
+        link: "https://myanimelist.net/anime/34599/Violet_Evergarden",
+        tags: "drama romance histórico"
+    },
+    {
+        titulo: "Akira",
+        descricao: "Um filme de animação cyberpunk que explora temas como a mutação genética e o poder da juventude, ambientado em um futuro distópico de Tóquio.",
+        link: "https://myanimelist.net/anime/351",
+        tags: "ação sci-fi cyberpunk"
+    },
+    {
+        titulo: "Cowboy Bebop",
+        descricao: "Cowboy Bebop é uma obra-prima da animação japonesa que nos transporta para um futuro distante, onde a humanidade colonizou o espaço. A bordo da nave espacial Bebop, uma equipe de caçadores de recompensas desbrava a galáxia em busca de fugitivos, enquanto lidam com seus próprios demônios e buscam um lugar para chamar de lar.",
+        link: "https://myanimelist.net/anime/1",
+        tags: "ação sci-fi drama space opera"
+    },
 ];
